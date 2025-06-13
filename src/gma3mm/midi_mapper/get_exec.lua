b62b2f661ba554ab8e6d692bda349bb32f6babfe
naneko <hello@kierasaltz.com>
2025-06-13 17:48:07 -0400

    Page data is now accessed directly and won't affect user page selection

--- src/gma3mm/midi_mapper/get_exec.lua
+++ src/gma3mm/midi_mapper/get_exec.lua
@@ -1,6 +1,19 @@
-Cmd("Page [[page]]")
-local cue_num = GetExecutor([[exec]]).object:Get("cueno")
+local page = Root():Children()[13]:Children()[14]:Children()[1]:Children()[12]:Children()[[[page]]]
+if page == nil then
+    return
+end
+local exec = nil
+for i = 1, #page:Children() do
+    if page:Children()[i].index == [[exec]] then
+        exec = page:Children()[i]
+        break
+    end
+end
+if exec == nil then
+    return
+end
+local cue_num = exec.object:Get("cueno")
 if cue_num == "" or cue_num == nil then
     cue_num = "None"
 end
-Cmd("SendOSC 2 \"/[[uid]],issfs," .. GetExecutor([[exec]]).index .. "," .. GetExecutor([[exec]]).key .. "," .. GetExecutor([[exec]]).fader .. "," .. GetExecutor([[exec]]):GetFader({}) .. "," .. cue_num .. "\"")
+Cmd("SendOSC 2 \"/[[uid]],issfs," .. exec.index .. "," .. exec.key .. "," .. exec.fader .. "," .. exec:GetFader({}) .. "," .. cue_num .. "\"")
